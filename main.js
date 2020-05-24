c0cb305ff1540c3c389ef9c1d1148a5be742036d
NaveenBandarage <thebanterage@gmail.com>
2020-05-25 02:48:20 +1200

    Pushing files

---
+++ main.js
@@ -0,0 +1,2 @@
+var endpoint = "https://jsonbox.io/box_db8e7fc4a952385abf38";
+function getrandom(){    var random_string = Math.random().toString(32).substring(2, 5) + Math.random().toString(32).substring(2, 5);    return random_string()}
